f00c2c74def9632c4cee0e855ae4274aab658c14
pcc-git <cannonp@byui.edu>
2024-09-30 18:05:22 -0600

    updated yamls for student work

--- _create_qmd_workbook.R
+++ _create_qmd_workbook.R
@@ -77,10 +77,12 @@ zip_folder <- function(folder_path, output_zip_name) {
 
 # Main script
 source_dirs <- c("./1-Data_Literacy", "./2-Tidy_Data", "./3-Stat_Fundamentals", "./4-Statistical_Tests_Part1", "5-Statistical_Tests_Part2", "./6-Semester_Project")
+
 yaml_to_add <- list(
   format = list(
     html = list(
-      self_contained = TRUE
+      self_contained = TRUE,
+      embed_resources = TRUE
     )
   )
 )
